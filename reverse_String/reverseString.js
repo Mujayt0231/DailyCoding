d8a03db37d0daea84cbc5448835f40fd9300897b
Mujayt Abdu <Mujiayitiabdu@gmail.com>
2022-11-14 11:29:43 -0500

    ReversedString

---
+++ reverse_String/reverseString.js
@@ -0,0 +1,40 @@
+const apple = 'apple';
+
+// function reversed(input) {
+//     return input.split('').reverse().join('');
+// }
+
+// console.log(reversed(apple))
+
+// function reversed(input) {
+
+//     let reversed = '';
+
+//     for (let character of input) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+// console.log(reversed(apple));
+
+
+
+
+
+
+// function reverse(input) {
+
+//     const arr = input.split('');
+//     arr.reverse();
+//     // const reverse = Array.from(input).reverse().join('');
+//     return arr.join('');
+// }
+// function reverse2(input) {
+
+//     return input.split('').reverse().join('');
+// }
+
+// console.log(reverse2(apple));
+// console.log(reverse(apple));
